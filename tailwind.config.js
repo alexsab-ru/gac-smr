8738f4f861d02202df126b2f853ccc5ad8302c2f
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-07-27 11:13:30 +0500

    models section

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,10 @@ export const theme = {
     gray: colors.gray,
   },
   extend: {
+    transitionTimingFunction: {
+      'in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
+      'out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
+    },
     fontFamily: {
       'sans': ['Roboto', ...defaultTheme.fontFamily.sans],
     },
