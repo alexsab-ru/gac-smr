bb8c2f2a7f5b7bf20dbb6cc5ead865b0f0b90932
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-10-13 10:30:26 +0500

    scroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
+import './modules/scroll';
 import './modules/analytics';
 import './modules/slider';
 import './modules/functions';
-import './modules/scroll';
 import './modules/color.links';
 import './modules/map';
 import './modules/form';
